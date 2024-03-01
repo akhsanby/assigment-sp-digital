ff5591f494581f6323506912d5b0e23de2aa76f6
akhsanby <akhsanby@gmail.com>
2024-03-01 21:54:06 +0700

    refactor code

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import svgLoader from 'vite-svg-loader';
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    svgLoader()
-  ],
+  plugins: [vue(), vueJsx(), svgLoader()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
