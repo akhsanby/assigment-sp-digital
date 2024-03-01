11f0ea06f6a326b490677c708c1d9dce0db1e3a9
akhsanby <akhsanby@gmail.com>
2024-03-01 23:14:28 +0700

    add new state & store

--- src/stores/index.js
+++ src/stores/index.js
@@ -4,14 +4,65 @@ export const useTableDataStore = defineStore("tableData", {
   state: () => {
     return {
       tableData: [],
+      searchResult: [],
     };
   },
   getters: {
     setTableData(state) {
       return (data) => (state.tableData = data);
     },
+    setSearchResult(state) {
+      return (data) => (state.searchResult = data);
+    },
   },
   persist: {
     storage: sessionStorage,
+  },
+});
+
+export const useModalStore = defineStore("modal", {
+  state: () => {
+    return {
+      showRemove: false,
+      showUpdate: false,
+    };
+  },
+  getters: {
+    closeRemove(state) {
+      return (data) => {
+        state.showRemove = false;
+        document.querySelector("#remove-modal").classList.remove("bg-gray-900/50");
+        document.querySelector("#remove-modal").classList.remove("fixed");
+        document.querySelector("#remove-modal").classList.remove("inset-0");
+        document.querySelector("#remove-modal").classList.remove("z-40");
+      };
+    },
+    closeUpdate(state) {
+      return (data) => {
+        state.showUpdate = false;
+        document.querySelector("#update-modal").classList.remove("bg-gray-900/50");
+        document.querySelector("#update-modal").classList.remove("fixed");
+        document.querySelector("#update-modal").classList.remove("inset-0");
+        document.querySelector("#update-modal").classList.remove("z-40");
+      };
+    },
+    toggleRemove(state) {
+      return (data) => {
+        state.showRemove = !state.showRemove;
+        document.querySelector("#remove-modal").classList.toggle("bg-gray-900/50");
+        document.querySelector("#remove-modal").classList.toggle("fixed");
+        document.querySelector("#remove-modal").classList.toggle("inset-0");
+        document.querySelector("#remove-modal").classList.toggle("z-40");
+      };
+    },
+    toggleUpdate(state) {
+      return (data) => {
+        state.showUpdate = !state.showUpdate;
+        document.querySelector("#update-modal").classList.toggle("bg-gray-900/50");
+        document.querySelector("#update-modal").classList.toggle("fixed");
+        document.querySelector("#update-modal").classList.toggle("inset-0");
+        document.querySelector("#update-modal").classList.toggle("z-40");
+      };
+    },
   },
 });
