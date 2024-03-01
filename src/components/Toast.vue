c65d81529ea400d920e6dbbfc59ba017fbcb5937
akhsanby <akhsanby@gmail.com>
2024-03-02 01:57:29 +0700

    initial toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="toast-top-right" class="fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+    <div class="text-sm font-normal">Top right positioning.</div>
+  </div>
+</template>
