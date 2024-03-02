d5d05be4982032cc03e472a38bd2122f14fae3b0
akhsanby <akhsanby@gmail.com>
2024-03-02 11:21:05 +0700

    toast component show total

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,5 +1,12 @@
+<script setup>
+import { useTableDataStore } from "@/stores";
+import { storeToRefs } from "pinia";
+
+const { checkedData } = storeToRefs(useTableDataStore());
+</script>
+
 <template>
-  <div id="toast-top-right" class="fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
-    <div class="text-sm font-normal">Top right positioning.</div>
+  <div :class="checkedData > 0 ? 'flex' : 'hidden'" id="toast-top-right" class="fixed items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+    <div class="text-sm font-normal">{{ checkedData }} selected</div>
   </div>
 </template>
